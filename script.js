6e091238a91c26e918d6b7400a270c6618994768
sne1423 <sneha1423@protonmail.com>
2024-07-18 07:59:25 +0000

    fgfg

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const now =new Date();
 const ourTime=now.getHours()+":"+now.getMinutes()+" PM GMT+5:30";
+// const ourTime=`${now.getHours()}:${now.getMinutes()} PM GMT+5:30`;
 console.log(ourTime);
 // show time
 const timeE1=document.querySelector(".time");
